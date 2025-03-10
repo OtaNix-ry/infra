be21d72623e379f9706e7861215e942f3c55a7be
SomeoneSerge <else@someonex.net>
2025-03-10 15:25:35 +0000

    flake: make free

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
@@ -28,23 +28,24 @@
     devShells =
       forEachSystem
       (system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
       in {
         default = devenv.lib.mkShell {
           inherit inputs pkgs;
           modules = [
             {
               # https://devenv.sh/reference/options/
-              packages = [pkgs.azure-cli];
+              packages = [
+                pkgs.azure-cli
+                pkgs.opentofu
+              ];
               languages.terraform.enable = true;
+              languages.terraform.package = pkgs.opentofu;
 
               git-hooks.hooks.terraform-fmt = {
                 enable = true;
                 name = "Terraform fmt check";
-                entry = "terraform fmt --recursive";
+                entry = "tofu fmt --recursive";
                 pass_filenames = false;
               };
             }
