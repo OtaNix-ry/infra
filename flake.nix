a2c1a37e7eda631f8bbd550504a70e459711580b
SomeoneSerge <else@someonex.net>
2025-03-10 17:23:44 +0000

    chore(flake): nixfmt

--- flake.nix
+++ flake.nix
@@ -11,53 +11,59 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    devenv,
-    systems,
-    ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = forEachSystem (system: {
-      devenv-up = self.devShells.${system}.default.config.procfileScript;
-      devenv-test = self.devShells.${system}.default.config.test;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
 
-      azure-image = (nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./image/configuration.nix
-        ];
-      }).config.system.build.azureImage;
-    });
-
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              # https://devenv.sh/reference/options/
-              packages = [
-                pkgs.azure-cli
-                pkgs.opentofu
-              ];
-              languages.terraform.enable = true;
-              languages.terraform.package = pkgs.opentofu;
-
-              git-hooks.hooks.terraform-fmt = {
-                enable = true;
-                name = "Terraform fmt check";
-                entry = "tofu fmt --recursive";
-                pass_filenames = false;
-              };
-            }
-          ];
-        };
+        azure-image =
+          (nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./image/configuration.nix
+            ];
+          }).config.system.build.azureImage;
       });
-  };
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = [
+                  pkgs.azure-cli
+                  pkgs.opentofu
+                ];
+                languages.terraform.enable = true;
+                languages.terraform.package = pkgs.opentofu;
+
+                git-hooks.hooks.terraform-fmt = {
+                  enable = true;
+                  name = "Terraform fmt check";
+                  entry = "tofu fmt --recursive";
+                  pass_filenames = false;
+                };
+              }
+            ];
+          };
+        }
+      );
+    };
 }
